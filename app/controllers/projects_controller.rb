e5e9b3e572e8747d1fc3b23adbd4bc8e7984d290
nicolas chevalier <nicolas_chevalier08@hotmail.fr>
2020-08-25 11:59:54 +0200

    All routes, views and controller created for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,58 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
+  end
+
+  def show
+    # @project = Project.find(params[:id])
+  end
+
+  def edit
+    # @project = Project.find(params[:id])
+  end
+
+  def update
+    # @project = Project.find(params[:id])
+    @project.update(params_project)
+    @project.user = current_user
+    @project.save
+
+    redirect_to project_path
+  end
+
+  def new
+    @user = current_user
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params_project)
+    @user = current_user
+
+    @project.user = @user
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    # @project = Project.find(params[:id])
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
+  private
+
+  def params_project
+    params.require(:project).permit(:title, :description, :github_repository, :trello_link, :start_date, :website_link, :tag)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
