fe53bcee56176e92a52168901b7c98bcfe057d64
Jules Roussel <julesroussel.de.c@gmail.com>
2020-08-25 14:22:41 +0200

    stand by

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,62 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
-html = Language.new(name: "HTML5")
+html = Language.new(name: "HTML/CSS")
 html_user = open("https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Font_Awesome_5_brands_html5.svg/1200px-Font_Awesome_5_brands_html5.svg.png")
 html.photo.attach(io: html_user, filename: 'html_file.png')
 html.save
+
+
+php = Language.new(name: "PHP")
+php_user = open("https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Font_Awesome_5_brands_php.svg/1279px-Font_Awesome_5_brands_php.svg.png")
+php.photo.attach(io: php_user, filename: 'php_file.png')
+php.save
+
+c = Language.new(name: "C++")
+c_user = open("https://img.favpng.com/8/18/6/c-programming-language-logo-computer-programming-png-favpng-mpuXd11Ye3fT7N8FTMjb7FHQy.jpg")
+c.photo.attach(io: c_user, filename: 'c_file.png')
+c.save
+
+js = Language.new(name: "JavaScript")
+js_user = open("https://img.favpng.com/2/12/22/javascript-icon-png-favpng-ruDBDhxzVxWHgXXtH2Hi1XzJf.jpg")
+js.photo.attach(io: js_user, filename: 'js_file.png')
+js.save
+
+ruby = Language.new(name: "Ruby")
+ruby_user = open("https://img.favpng.com/4/13/16/ruby-on-rails-logo-programming-language-rubygems-png-favpng-WhQbCrZxcK4rVV4XP3x5JFYTF.jpg")
+ruby.photo.attach(io: ruby_user, filename: 'ruby_file.png')
+ruby.save
+
+sql = Language.new(name: "SQL")
+sql_user = open("https://e7.pngegg.com/pngimages/300/160/png-clipart-computer-icons-font-awesome-user-font-logo-database-angle-software-development-thumbnail.png")
+sql.photo.attach(io: sql_user, filename: 'sql_file.png')
+sql.save
+
+python = Language.new(name: "Python")
+python_user = open("https://img2.freepng.fr/20180320/fkq/kisspng-angle-text-symbol-brand-other-python-5ab0c09b32b4d1.7494578715215330832077.jpg")
+python.photo.attach(io: python_user, filename: 'python_file.png')
+python.save
+
+react = Language.new(name: "React")
+react_user = open("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png")
+react.photo.attach(io: react_user, filename: 'react_file.png')
+react.save
+
+java = Language.new(name: "Java")
+java_user = open("https://user-images.githubusercontent.com/20663500/38591695-8e84298c-3d6a-11e8-8f84-71113c79c336.png")
+java.photo.attach(io: java_user, filename: 'java_file.png')
+java.save
+
+swift = Language.new(name: "Swift")
+swift_user = open("https://img.favpng.com/9/1/24/swift-apple-programming-language-macos-png-favpng-zpZe4qRiZ0qjPeekRDddixMp8.jpg")
+swift.photo.attach(io: swift_user, filename: 'swift_file.png')
+swift.save
+
+angular = Language.new(name: "Angular")
+angular_user = open("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Font_Awesome_5_brands_angular.svg/1200px-Font_Awesome_5_brands_angular.svg.png")
+angular.photo.attach(io: angular_user, filename: 'angular_file.png')
+angular.save
+
+
+
+
