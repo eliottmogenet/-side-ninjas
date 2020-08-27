3c102e8570351fec74691fc792df7b9845603d41
Eliott Mogenet <eliott.mogenet@gmail.com>
2020-08-27 11:01:11 +0200

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,7 @@ class User < ApplicationRecord
   has_many :projects
   has_many :participations, dependent: :destroy
   has_many :projects, through: :participations
-<<<<<<< HEAD
   has_many :user_languages, dependent: :destroy
   validates :first_name, :last_name, :batch_number, :city, :bootcamp_year, presence: true
-=======
-  has_many :user_languages
   has_one_attached :photo
-  validates :first_name, :last_name, :batch_number, :city, :pays, :bootcamp_year, presence: true
->>>>>>> master
 end
