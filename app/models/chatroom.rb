7dd44e2539d9da1af329027e23e9514b127d2a83
Adrien Fort <adrien.fort01@gmail.com>
2020-09-02 00:45:21 +0200

    un nom est donnée au conversations

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,6 @@
 class Chatroom < ApplicationRecord
-  has_many :messages
-  has_many :chatroom_users
+  has_many :messages, dependent: :destroy
+  has_many :chatroom_users, dependent: :destroy
   has_many :users, through: :chatroom_users
   # belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   # belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
